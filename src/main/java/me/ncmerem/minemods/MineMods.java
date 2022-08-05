ef4133083b3f02a11f6ce700e9398b34dc85ed2c
ncmerem <chinumdrum@gmail.com>
2022-08-05 16:45:20 -0500

    finally figured out how to fix her git credentials

--- src/main/java/me/ncmerem/minemods/MineMods.java
+++ src/main/java/me/ncmerem/minemods/MineMods.java
@@ -17,7 +17,7 @@ public class MineMods {
     public static final String MOD_ID = "minemods";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Very Important Comment 3: Please be the Finale
+    // Very Important Comment 4: Resurrection
 
     public MineMods() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
